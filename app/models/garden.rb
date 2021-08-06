2bb5bb5d3aaaeea3ebf25687141bd94f1ac4aae8
Matt Kragen <mattdkragen@gmail.com>
2021-08-06 09:59:51 -0700

    Write method for ordering and test

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,4 +1,14 @@
 class Garden < ApplicationRecord
   has_many :plots
   has_many :plants, through: :plots
+
+  def plant_display
+    self.plants
+    .joins(:plant_plots)
+    .select('plants.*, count(plant_plots.plant_id) as plot_count')
+    .where('days_to_harvest < 100')
+    .group('plants.id')
+    .order('plot_count desc')
+    .distinct
+  end
 end
